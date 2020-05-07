652d376059059a7f479ab61bc485aca04590d7aa
Aoife Tjärnlund <tjarnlunda@gmail.com>
2020-05-07 11:04:53 +0200

    geolocation på rätt plats

--- klientprogrammering/src/components/geolocation.js
+++ klientprogrammering/src/components/geolocation.js
@@ -1,17 +1,18 @@
 function locate(){
-    const position = document.querySelector('#posStatus');
+    const position = document.querySelector('#position');
     const locInfo = document.querySelector('#locInfo');
 
     position.innerHTML='Locating...'
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) =>{
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
+            const latitude=position.coords.latitude;
+            const longitude=position.coords.longitude;
 
             position.innerHTML='Location';
-            locInfo.innerHTML = 'Latitude: ${latitude}, Longitude: ${longitude}';
+            locInfo.innerHTML= ` Latitude: ${latitude}, Longitude: ${longitude}` ;
             
+
 
         })
     }
