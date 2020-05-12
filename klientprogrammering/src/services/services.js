142fa7c05a563b63d49a10e8491ad7b70a6a6092
ElskeM <e.muijderman@gmail.com>
2020-05-12 09:14:15 +0200

    lade till area och location i services

--- klientprogrammering/src/services/services.js
+++ klientprogrammering/src/services/services.js
@@ -1,6 +1,36 @@
 function getEventsNearby(url) {
     return fetch(url)
-    .then(resp => {
+        .then(resp => { /* await */
+            if (!resp.ok) {
+                throw new Error(resp.statusText)
+            }
+            return resp.json()
+        })
+        .then(json => {
+            console.log(json.data)
+            console.log(json.data[0])
+        }
+    /*json.map(h => h.handelse)*/)
+}
+
+function getEventsByLocation(url) {
+    return fetch(url)
+        .then(resp => { 
+            if (!resp.ok) {
+                throw new Error(resp.statusText)
+            }
+            return resp.json()
+        })
+        .then(json => {
+            console.log(json.data)
+            console.log(json.data[0])
+        }
+    /*json.map(h => h.handelse)*/)
+}
+
+function getEventsByArea(url) {
+    return fetch(url)
+    .then(resp => { 
         if (!resp.ok) {
             throw new Error(resp.statusText)
         }
@@ -14,7 +44,10 @@ function getEventsNearby(url) {
 }
 
 const BrottsplatsService = {
-    eventsNearby: () => getEventsNearby('https://brottsplatskartan.se/api/eventsNearby?lat=57.70&lng=11.96')
+    eventsNearby: () => getEventsNearby('https://brottsplatskartan.se/api/eventsNearby?lat=57.70&lng=11.96'),
+    eventsByLocation: () => getEventsByLocation('https://brottsplatskartan.se/api/events/?location=göteborg'),
+    eventsByArea: () => getEventsByArea('https://brottsplatskartan.se/api/events/?area=västra götalands län')
+
 }
 Object.freeze(BrottsplatsService)
 
