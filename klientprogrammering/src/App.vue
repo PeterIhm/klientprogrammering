a9e49ce42690a2f8384df399ccc24b8112c91d17
ElskeM <e.muijderman@gmail.com>
2020-05-12 10:47:09 +0200

    lade till tomma vue sidor till olika vyer

--- klientprogrammering/src/App.vue
+++ klientprogrammering/src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>|
+      <router-link to="/eventsnearby">Events nearby</router-link>
+      <router-link to="/eventsbylocation">Events by location</router-link>
+      <router-link to="/eventsbyarea">Events by area</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
