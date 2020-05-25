ede09c269fcc4b6c2ab63a22797b7703f8766562
Aoife Tjärnlund <tjarnlunda@gmail.com>
2020-05-25 14:22:22 +0200

    mer toast

--- klientprogrammering/src/components/ErrorToast.js
+++ klientprogrammering/src/components/ErrorToast.js
@@ -1,7 +1,6 @@
 import Toasted from 'vue-toasted';
   vue.use(Toasted, Options)
 
-function ErrorToast(){
-  this.$toasted.show('Vi kan tyvärr inte hitta din plats')
 
-}
+  let minToast = this.$toasted.show('Vi kan tyvärr inte hitta din plats')
+
