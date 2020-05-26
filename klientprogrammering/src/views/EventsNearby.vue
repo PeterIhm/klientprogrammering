d7a44b24318529a0bc0e52d3aca2a9cd0850280c
ElskeM <e.muijderman@gmail.com>
2020-05-26 20:05:18 +0200

    lade till anrop till store i eventsnearby

--- klientprogrammering/src/views/EventsNearby.vue
+++ klientprogrammering/src/views/EventsNearby.vue
@@ -37,21 +37,45 @@
 </template>
 
 <script>
-
-import BrottsplatsService from '@/services/services.js'
 export default {
-  mounted(){
-    BrottsplatsService.getEventsNearby().then(data => console.log(data))
-
+  mounted() {
+    this.$store.dispatch("eventsNearby");
   }
-}
+};
 </script>
 
 <style>
-
-.search-box{
+.search-box {
   background-color: #822c57;
 }
-
-
+.sibling-hover,
+#parent {
+  width: 100%;
+  border: none;
+  color: white;
+  background-color: #00000050;
+  outline: none;
+  padding: 1em;
+  font-size: 1em;
+}
+.sibling-hover ~ .sibling-highlight {
+  display: none;
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.sibling-hover:hover ~ .sibling-highlight {
+  display: block;
+  color: white;
+}
+.content >>> p {
+  margin-left: 0.8em;
+  margin-right: 0.5em;
+  text-align: left;
+  text-shadow: 2px 2px #0000005b;
+}
+.date >>> p {
+  margin-left: 0.8em;
+  margin-right: 0.5em;
+  text-align: right;
+  text-shadow: 2px 2px #0000005b;
+}
 </style>
