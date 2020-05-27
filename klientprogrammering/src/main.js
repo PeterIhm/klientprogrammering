74e21d0a56bdd847833077e56d361cab49700b7d
OnlySchnitzel <peter.ihm98@gmail.com>
2020-05-27 18:07:21 +0200

    Fixad saken med Component....

--- klientprogrammering/src/main.js
+++ klientprogrammering/src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import eventlist from './components/EventList.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  app,
+  render: h => h(App),
 }).$mount('#app')
+
+
+Vue.component('event-list', eventlist)
+
+let app = new Vue({
+  el: '#eventlist'
+})
